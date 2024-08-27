dddbd8cba2ec5c7a3a07140b58d58f52040a0ce2
Derek Yee <derek.s.yee@gmail.com>
2024-08-27 09:05:30 -1000

    jquery

--- docs.js
+++ docs.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var collapseOne = document.getElementById('collapseOne');
-    if (collapseOne) {
-        collapseOne.addEventListener('hidden.bs.collapse', function () {
-            var childAccordions = document.querySelectorAll('#collapseOne .accordion-collapse');
-            childAccordions.forEach(function (accordion) {
-                if (accordion.classList.contains('show')) {
-                    var collapseInstance = bootstrap.Collapse.getInstance(accordion);
-                    if (collapseInstance) {
-                        collapseInstance.hide();
+    for (let i = 1; i<4; i++){
+        var collapse = document.getElementById(`collapse${i}`);
+        if (collapse) {
+            collapse.addEventListener('hidden.bs.collapse', function () {
+                var childAccordions = document.querySelectorAll(`#collapse${i} .accordion-collapse`);
+                childAccordions.forEach(function (accordion) {
+                    if (accordion.classList.contains('show')) {
+                        var collapseInstance = bootstrap.Collapse.getInstance(accordion);
+                        if (collapseInstance) {
+                            collapseInstance.hide();
+                        }
                     }
-                }
+                });
             });
-        });
+        }
     }
+    $(function(){
+        $("#lossesAccordion").load("lossesAccordion.html");
+      });
 });

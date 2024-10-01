4f247ccf649c482737e368f39652a7dfec6e723f
Derek Yee <derek.s.yee@gmail.com>
2024-09-30 20:44:25 -1000

    port 5001

--- application_test/app.py
+++ application_test/app.py
@@ -34,4 +34,4 @@ def process_data():
     return jsonify(returnPictureData)
 
 if __name__ == '__main__':
-    app.run(app.run(host='45.63.57.237', port=80))
+    app.run(app.run(host='45.63.57.237', port=5001))
